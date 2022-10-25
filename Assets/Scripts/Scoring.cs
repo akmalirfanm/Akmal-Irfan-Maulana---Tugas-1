053b6c7444f186dca4770daa68d17908a66ab718
Akmal Irfan Maulana <akmalirfanm@gmail.com>
2022-10-25 15:19:20 +0700

    mod5

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -6,18 +6,11 @@ using UnityEngine.UI;
 public class Scoring : MonoBehaviour
 {
     public Text Myscore;
-    public int ScoreNum;
-    // Start is called before the first frame update
+    private int ScoreNum;
     void Start()
     {
         ScoreNum = 0;
         Myscore.text = "Score : " + ScoreNum;
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
 
